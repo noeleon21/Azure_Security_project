6621540e492d9086019145ac1becb4dc30d1d7c6
noeleon21 <noelgens@gmail.com>
2025-10-30 20:56:51 +1100

    install webserver again

---
+++ Other_tf_files/outputs.tf
@@ -0,0 +1,6 @@
+
+# resource "azurerm_security_center_subscription_pricing" "mdc_storage" {
+#   tier          = "Free"
+#   resource_type = "StorageAccounts"
+#   subplan       = "DefenderForStorageV2"
+# }
