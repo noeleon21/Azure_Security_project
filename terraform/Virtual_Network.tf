40d3e48098e596ba9ebf8668361be7044eefd89d
noeleon21 <noelgens@gmail.com>
2026-05-24 17:25:18 +1000

    enable rdp port

--- terraform/Virtual_Network.tf
+++ terraform/Virtual_Network.tf
@@ -7,6 +7,20 @@ resource "azurerm_network_security_group" "azure_security_nsg" {
   name                = "azure-security-nsg"
   location            = azurerm_resource_group.azure_security_rg.location
   resource_group_name = azurerm_resource_group.azure_security_rg.name
+
+
+
+  security_rule {
+    name                       = "AllowRDP"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 resource "azurerm_virtual_network" "azure_security_vnet" {
